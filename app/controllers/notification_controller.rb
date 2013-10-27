3de8cffeb980387e9d8c30424395f3d9cdbc6013
Ryan Endacott <rzeg24@gmail.com>
2013-10-26 21:19:28 -0500

    better text message

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -5,14 +5,14 @@ class NotificationController < ApplicationController
   def notify
     if ENV['send'] == 'do it!!!'
       email_status = NotifyMailer.notify_email(params[:email],  params[:name], params[:website], params[:original_content], params[:new_content]).deliver
-      text_status = send_twilio_notification(params[:phone_number])
+      text_status = send_twilio_notification(params[:phone_number], params[:website])
       render json: { email: true, sms: true}
     else
       render json: {status: 'not sent because of config vars'}
     end
   end
 
-  def send_twilio_notification(number = '+14179880783')
-    @@client.account.sms.messages.create(body: 'Temporary body', to: number, from: ENV['TWILIO_NUMBER'])
+  def send_twilio_notification(number, url)
+    @@client.account.sms.messages.create(body: "ChangeNotifier alert on #{url}!  The content has changed.", to: number, from: ENV['TWILIO_NUMBER'])
   end
 end
