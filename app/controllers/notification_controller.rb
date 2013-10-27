11803b97ba328583a7b5274c15550f3096b2fcce
Ryan Endacott <rzeg24@gmail.com>
2013-10-27 08:55:44 -0500

    hi

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -20,15 +20,15 @@ class NotificationController < ApplicationController
 
   def notify
     if ENV['send'] == 'do it!!!'
-      email_status = NotifyMailer.notify_email(params[:email],  params[:name], params[:website], params[:original_content], params[:new_content]).deliver
-      text_status = send_twilio_notification(params[:phone_number], params[:website])
+      email_status = NotifyMailer.notify_email(params[:email],  params[:name], params[:monitor_name], params[:original_content], params[:new_content]).deliver
+      text_status = send_twilio_notification(params[:phone_number], params[:monitor_name])
       render json: { email: true, sms: true}
     else
       render json: {status: 'not sent because of config vars'}
     end
   end
 
-  def send_twilio_notification(number, url)
-    @@client.account.sms.messages.create(body: "ChangeNotifier alert on #{url}!  The content has changed.", to: number, from: ENV['TWILIO_NUMBER'])
+  def send_twilio_notification(number, monitor_name)
+    @@client.account.sms.messages.create(body: "ChangeNotifier alert on #{monitor_name}!  The content has changed.", to: number, from: ENV['TWILIO_NUMBER'])
   end
 end
